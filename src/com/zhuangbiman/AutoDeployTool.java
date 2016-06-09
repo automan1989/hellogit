15f5b5bc8d11c39da80bc0432eed3e0fa420d50d
zhuangbiman <automan1989@gmail.com>
2016-06-09 23:19:28 +0800

    auto deploy tool

---
+++ src/com/zhuangbiman/AutoDeployTool.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.zhuangbiman;
+
+/**
+ * @author Administrator
+ *
+ */
+public class AutoDeployTool {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
